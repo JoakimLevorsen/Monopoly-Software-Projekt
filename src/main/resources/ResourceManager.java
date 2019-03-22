36bf575b2d1489604292f21b5bdb8391f5b9b1be
Joakim <ijoakim@me.com>
2019-03-22 10:31:44 +0100

    readFile tilføjet

--- src/main/resources/ResourceManager.java
+++ src/main/resources/ResourceManager.java
@@ -1,9 +1,21 @@
 package resources;
 
 import org.json.*;
+import org.apache.commons.io.IOUtils;
 
 public class ResourceManager {
-    public JSONObject readFile() {
-        
+    public JSONObject readFile(JSON_FILE file) {
+        file.fileName();
+
+        String result = "";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        try {
+            result = IOUtils.toString(classLoader.getResourceAsStream(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
