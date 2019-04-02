423d0ee7ffff957c91dcba7b446977b5e74f3280
Joakim Levorsen <joakim.levorsen@icloud.com>
2019-04-02 14:13:04 +0200

    Rename

--- src/main/java/monopoly/model/spaces/DatabaseSpaceFactory.java
+++ src/main/java/monopoly/model/spaces/DatabaseSpaceFactory.java
@@ -9,12 +9,12 @@ import monopoly.model.Game;
 public class DatabaseSpaceFactory {
     public static List<Space> getSpacesFor(Game game) {
 
-        Class<? extends Space>[] arr = new Class[] { CardSpace.class, FreeParkingSpace.class, GoToJailSpace.class,
+        Class<? extends Space>[] classArray = new Class[] { CardSpace.class, FreeParkingSpace.class, GoToJailSpace.class,
                 JailSpace.class, PropertySpace.class, StartSpace.class, StationSpace.class };
 
         // Get the spaces
         List<Space> allSpaces = new ArrayList<Space>();
-        for (Class<? extends Space> c : arr) {
+        for (Class<? extends Space> c : classArray) {
             allSpaces.addAll(game.getAll(c).load());
         }
 
