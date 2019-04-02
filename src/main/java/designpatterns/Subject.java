8e6e37fdd0d2061142c28539c78ef3c2f0d9f804
Joakim Levorsen <joakim.levorsen@icloud.com>
2019-04-02 14:59:51 +0200

    Nyt subject interface

--- src/main/java/designpatterns/Subject.java
+++ src/main/java/designpatterns/Subject.java
@@ -1,49 +1,15 @@
 package designpatterns;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.javalite.activejdbc.Model;
+import java.util.List;
 
-/**
- * This is the subject of the observer design pattern roughly following
- * the definition of the GoF.
- * 
- * @author Ekkart Kindler, ekki@dtu.dk
- *
- */
-public abstract class Subject extends Model {
-	
-	private Set<Observer> observers = new HashSet<Observer>();
-	
-	/**
-	 * This methods allows an observer to register with the subject
-	 * for update notifications when the subject changes.
-	 * 
-	 * @param observer the observer who registers
-	 */
-	final public void attach(Observer observer) {
-		observers.add(observer);
-	}
-	
-	/**
-	 * This methods allows an observer to unregister from the subject
-	 * again.
-	 * 
-	 * @param observer the observer who unregisters
-	 */
-	final public void detach(Observer observer) {
-		observers.remove(observer);
-	}
-	
-	/**
-	 * This method must be called from methods of concrete subclasses
-	 * of this subject class whenever its state is changed (in a way
-	 * relevant for the observer).
-	 */
-	final protected void notifyChange() {
-		for (Observer observer: observers) {
-			observer.update(this);
-		}
-	}
+public interface Subject {
+    public void addObserver(Observer observer);
 
+    public void removeObserver(Observer observer);
+
+    public List<Observer> getObservers();
+
+    public default void updated() {
+        for (Observer o : this.getObservers()) o.update(this);
+    }
 }
