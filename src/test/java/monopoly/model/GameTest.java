15b1517b4bba59a5f1cd2b65295392f463d41128
Joakim Levorsen <joakim.levorsen@icloud.com>
2019-04-02 16:26:23 +0200

    Game test fixed

--- src/test/java/monopoly/model/GameTest.java
+++ src/test/java/monopoly/model/GameTest.java
@@ -2,9 +2,18 @@ package monopoly.model;
 
 import static org.junit.Assert.assertNotEquals;
 
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
+    @Before
+    public void setupTest() {
+        Base.open("com.mysql.cj.jdbc.Driver", "jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185023",
+                "s185023", "t0MzfHeQBfHIlo8ociaB2");
+    }
+
     @Test
     public void testAllKeysPresent() {
         Game g = Game.newGame("TEST GAME SAFE TO DELETE");
@@ -14,5 +23,10 @@ public class GameTest {
         }
 
         g.deleteThisGame();
+    }
+
+    @After
+    public void endTest() {
+        Base.close();
     }
 }
