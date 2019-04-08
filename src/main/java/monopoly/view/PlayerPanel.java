31d0d8785c868e8ce3b704cd64689e814b414080
Cecilie Drejer <ceciliek.drejer@gmail.com>
2019-04-08 15:49:23 +0200

    Tilføjet tomme metoder + TODOS & rettet imports

---
+++ src/main/java/monopoly/view/PlayerPanel.java
@@ -0,0 +1,14 @@
+package monopoly.view;
+
+import designpatterns.*;
+import monopoly.model.Player;
+
+public class PlayerPanel {
+    public void update(Subject subject) {
+        //TODO    
+    }
+
+    public void update(Player player) {
+        //TODO
+    }
+}
--- src/main/java/monopoly/view/View.java
+++ src/main/java/monopoly/view/View.java
@@ -1,9 +1,21 @@
 package monopoly.view;
 
+import java.util.HashMap;
+
 import designpatterns.*;
+import monopoly.model.Player;
+import monopoly.model.spaces.*;
+import gui_fields.*;
 
 public class View implements Observer {
+    HashMap<Space, GUI_Field> spaceToField = new HashMap<Space, GUI_Field>();
+    HashMap<Player, GUI_Player> playerToGUIPlayer = new HashMap<Player, GUI_Player>();
+
     public void update(Subject subject) {
-        
+        //TODO    
+    }
+
+    public void update(Player player) {
+        //TODO
     }
 }
