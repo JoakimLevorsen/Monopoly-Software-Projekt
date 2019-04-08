c5be3a62f3e4801b1f1f1fbf03e1dbc7d9990307
Anders Brandt <s185016@student.dtu.dk>
2019-04-08 15:58:58 +0200

    Oprettet PlayerPanel klasse, og kopieret kode fra mini monopoly som skal rettes ind så det virker her.
    Har også tilføjet et par ting i view, der skal bruges til at opdatere PlayerPanel

--- src/main/java/monopoly/view/View.java
+++ src/main/java/monopoly/view/View.java
@@ -1,9 +1,13 @@
 package monopoly.view;
 
 import designpatterns.*;
+// Dette, eller noget lignende dette skal bruges til at opdatere playerpanel i view
+//private Map<Player,PlayerPanel> panels = new HashMap<>();
 
 public class View implements Observer {
     public void update(Subject subject) {
         
     }
 }
+//Dette skal være en del af updateplayer, for at opdatere panelerne.
+//panels.get(player).Update();
