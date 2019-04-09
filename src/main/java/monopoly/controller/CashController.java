a63754868d04be98f6b758e95ea54645e325c204
HellaAchari <hejmeddig123>
2019-04-09 14:19:21 +0200

    Tilføjet metoder for at tage penge fra en spiller og give til en anden spiller.

--- src/main/java/monopoly/controller/CashController.java
+++ src/main/java/monopoly/controller/CashController.java
@@ -10,17 +10,26 @@ public class CashController {
         this.controller = owner;
     }
 
-    public void offerProperty(PropertySpace property, Player player) {
+    public void offerProperty(PropertySpace property, Player player){
         // TODO: Implementering
 
     }
 
     public void payment(Player playerFrom, int amount, Player playerReceiver) {
-        // TODO: Implementering
+        playerFrom.changeAccountBalance(- amount);
+        playerReceiver.changeAccountBalance(amount);
+        if (playerFrom.getAccountBalance() < 0){
+
+
+
+        }
+        // TODO: Håndtering af konkurs
     }
 
     public void paymentFromBank(Player player, int amount) {
-        // TODO: Implementering
+        player.changeAccountBalance(amount);
+        // TODO: Håndtering af konkurs
+
 
     }
 }
