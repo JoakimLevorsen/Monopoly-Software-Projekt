bfb66cff8143d35a7d0229f32a2970c224ba41ed
Anders Brandt <s185016@student.dtu.dk>
2019-04-23 16:09:01 +0200

    implementeret i MovementController så der faktisk sker noget når man lander på et felt

--- src/main/java/monopoly/controller/MovementController.java
+++ src/main/java/monopoly/controller/MovementController.java
@@ -5,14 +5,24 @@ import monopoly.model.spaces.JailSpace;
 import monopoly.model.spaces.Space;
 import monopoly.model.spaces.StartSpace;
 
+
 public class MovementController {
     public GameController controller;
 
-
+    /*
+     * MovementController: kontrollerer de forskellige måder at rykke spilleren
+     *
+     * @Author Anders Brandt, s185016
+     */
     public MovementController(GameController owner) {
         this.controller = owner;
     }
 
+    /*
+     * moveForward: rykker spilleren frem ud fra hvad de har slået med terningen.
+     *
+     * @Author Anders Brandt, s185016
+     */
     public void moveForward(int amount, Player player) {
         int moveAmount = player.getBoardPosition() + amount;
 
@@ -22,17 +32,29 @@ public class MovementController {
             player.changeAccountBalance(s.getPayment());
         }
         player.setBoardPosition(moveAmount);
+        Space destination = controller.getGame().getBoard().get(player.getBoardPosition());
+        destination.performAction(controller, player);
     }
 
-
-        public void goToJail (Player player){
+    /*
+     * goToJail: rykker spilleren i fængsel
+     *
+     * @Author Anders Brandt, s185016
+     */
+        public void goToJail(Player player) {
             JailSpace j = (JailSpace) controller.getGame().getAll(JailSpace.class).load().get(0);
             player.setBoardPosition(j.getBoardPosition());
         }
 
-
-        public void goTo (Space space, Boolean ignoreStart, Player player){
+    /*
+     * goTo: rykker spilleren til et bestemt felt.
+     *
+     * @Author Anders Brandt, s185016
+     */
+        public void goTo(Space space, Boolean ignoreStart, Player player) {
             player.setBoardPosition(space.getBoardPosition());
+            space.performAction(controller, player);
             //TODO: Tjek for start kryds
         }
+
     }
