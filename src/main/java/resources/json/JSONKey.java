0e902c7a7e5d27db58fb2973fb03eabdf8f17c0a
Cecilie Drejer <ceciliek.drejer@gmail.com>
2019-05-15 15:20:12 +0200

    Oversat json

--- src/main/java/resources/json/JSONKey.java
+++ src/main/java/resources/json/JSONKey.java
@@ -7,7 +7,7 @@ Enum der indeholder alle JSON nøgler i projektet, for at undgå stavefejl mens 
 @author Joakim Levorsen, S185023
 */
 public enum JSONKey {
-    TEST("test"), START_PAYMENT("startPayment"), CURRENTLY_BROKE("currentlyBroke"), PLAYER_BROKE_TITLE("playerBrokeTitle"), SELL_PROPERTY("sellProperty"), 
+    TEST("test"), CURRENTLY_BROKE("currentlyBroke"), PLAYER_BROKE_TITLE("playerBrokeTitle"), SELL_PROPERTY("sellProperty"), 
     MORTGAGE_PROPERTY("mortgageProperty"), YOU_HAVE_CHOSEN("youHaveChosen"), WHAT_DO("whatDo"), HOUSES_ON_PROPERTY("housesOnProperty"), SPACES("spaces"), 
     NAME("name"), TYPE("type"), PRICE("price"),BASE_RENT("baseRent"), VALUE("value"), COLOR("color"), TEXT_COLOR("textColor"), IMAGE("image"),
     CHANCE_CARDS("chanceCards"), COMMUNITY_CHEST_CARDS("communityChestCards"), TEXT("text"), SPACE("space"),
