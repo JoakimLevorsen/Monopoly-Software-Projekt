bfdd9b56be2dad989465578fab8086b78e41fb96
Anders Brandt <s185016@student.dtu.dk>
2019-05-19 11:32:58 +0200

    lidt flere rettede author tags

--- src/main/java/monopoly/controller/CashController.java
+++ src/main/java/monopoly/controller/CashController.java
@@ -11,19 +11,35 @@ import monopoly.model.Player;
 public class CashController {
     public GameController controller;
 
+    /**
+     * @param owner
+     */
     public CashController(GameController owner) {
         this.controller = owner;
     }
 
+    /**
+     * @param playerFrom
+     * @param amount
+     * @param playerReceiver
+     */
     public void payment(Player playerFrom, int amount, Player playerReceiver) {
         playerFrom.changeAccountBalance(-amount);
         playerReceiver.changeAccountBalance(amount);
     }
 
+    /**
+     * @param player
+     * @param amount
+     */
     public void paymentToBank(Player player, int amount) {
         player.changeAccountBalance(-amount);
     }
 
+    /**
+     * @param player 
+     * @param amount
+     */
     public void paymentFromBank(Player player, int amount) {
         player.changeAccountBalance(amount);
     }
